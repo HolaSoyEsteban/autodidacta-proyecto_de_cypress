51c541b624a676d941a5f2027013e5953ecba819
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 11:59:39 -0300

    Probando metodo contains, para encontrar texto, accediendo a atributo en especifico y mostrando su valor

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,9 +1,11 @@
-describe('test case', () => {
+describe('test suite', () => {
     it('test case', () => {
-        cy.visit('https://www.saucedemo.com/') // Accede a la dirección del sitio indicado
-        cy.get('#user-name').as('username')    // Se le asigna un alias al Selector
-
-        cy.get('@username').type('hello')      // Se usa el alias y se le asigna un valor
-        cy.wait(10000)                         // Tiempo de espera para lograr ver la acción
+        cy.visit('https://www.saucedemo.com/')              // Accede a la dirección del sitio indicado.
+        cy.contains('div', 'Swag Labs').then(title => {     // Busca usando texto (al no poder usar un Selector CSS)
+                                                            // lo que encuentra lo trabaja en 'title'
+            
+            cy.log(title.attr('class'))                     // Accede al atributo 'class' de 'title' y muestra su valor
+        })
+        cy.wait(5000)                                       // Tiempo de espera
     })
 })
