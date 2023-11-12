d53330c76b7d6e6189f72b85aa81425304d99d7a
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 13:50:10 -0300

    Probando metodo should, con argumento contain, para verificar que contenga el valor solicitado

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,7 +1,8 @@
 describe('test suite', () => {
     it('test case', () => {
         cy.visit('https://www.saucedemo.com/')     // Accede a la dirección del sitio indicado.
-        cy.get('.login_logo').should('contain.text', 'Labs')    // Usa em metodo should, con el argumento contain, para verificar que contenga el texto solicitado
+        cy.get('#user-name').type('Navidad')       // Usa el metodo type, para insertar texto en el elemento
+        cy.get('#user-name').should('contain.value', 'idad')    // Usa el metodo should, con el argumento contain, para verificar que contenga el valor solicitado
         cy.wait(5000)
     })
 })
