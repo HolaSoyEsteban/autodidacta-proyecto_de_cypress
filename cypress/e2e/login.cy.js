6cfe12c812da294c1a5863d91367d6187fe6e767
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 11:31:04 -0300

    Primer caso de prueba - Probando el Login y selectores CSS

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+describe('Login', () => {
+    it('Login con credenciales standard user', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('#user-name').type('standard_user')
+        cy.get('#password').type('secret_sauce')
+        cy.get('#login-button').click()
+        cy.get('.title').should('exist')
+    })
+})
