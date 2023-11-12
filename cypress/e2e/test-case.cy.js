fb54bab464a68a2b3e5bfc82f252841d74e12cb8
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 12:49:56 -0300

    Probando metodo eq, selecciona posicion (en selectores indenticos) por indice (como un array)

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,7 +1,7 @@
 describe('test suite', () => {
     it('test case', () => {
         cy.visit('https://www.saucedemo.com/')       // Accede a la dirección del sitio indicado.
-        cy.get('.form_input').first().type('Hola')   // Usa metodo first para seleccionar el primer elemento de varios con la misma class
+        cy.get('.form_input').eq(1).type('Algo')   // Usa metodo eq para seleccionar (en objetos que repiten su selector) la posicion (como un array, su indice) dentro del scope
         cy.wait(5000)
     })
 })
