b65417c405b013dba81b20c379b42854a06622f9
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 13:31:18 -0300

    Probando metodo should, con argumento have, para verificar si el valor del elemento seleccionado corresponde a lo pedido

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,7 +1,8 @@
 describe('test suite', () => {
     it('test case', () => {
         cy.visit('https://www.saucedemo.com/')     // Accede a la dirección del sitio indicado.
-        cy.get('.login_logo').should('have.text', 'Swag Labs')    // Usa em metodo should, con el argumento have, verifica si su texto corresponde al indicado por parametro
+        cy.get('#user-name').type('Hola')    // Usa em metodo type, para introducir texto en ese selector
+        cy.get('#user-name').should('have.value', 'Hola') // Comprueba que ese selector tenga como valor , 'Hola'
         cy.wait(5000)
     })
 })
