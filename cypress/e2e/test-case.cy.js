e8eacf5d8b99c9b1cf14b29d95705233b70eb44c
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 21:38:27 -0300

    Probando metodo should, con argumento not.be.checked, para verificar que el elemento NO este seleccionado

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,7 +1,7 @@
 describe('test suite', () => {
     it('test case', () => {
         cy.visit('https://the-internet.herokuapp.com/checkboxes')     // Accede a la dirección del sitio indicado.
-        cy.get('input[type="checkbox"]').eq(1).should('be.checked')   // Usa el metodo eq para  seleccionar la ubicacion del elemento, luego el metodo should, con argumento 'be.checked' para chequear que el elemento este seleccionado
+        cy.get('input[type="checkbox"]').eq(0).should('not.be.checked')   // Usa el metodo eq para  seleccionar la ubicacion del elemento, luego el metodo should, con argumento (notese el 'not' que fue agregado) 'not.be.checked' para chequear que el elemento NO este seleccionado
         
         cy.wait(5000)
     })
