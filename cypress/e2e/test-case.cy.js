33f156f2ce627fe5ac36aadbd82dadb9994c0d63
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 21:23:12 -0300

    Probando metodo should, con argumento be.visible, para verificar que el elemento sea visible

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,8 +1,11 @@
 describe('test suite', () => {
     it('test case', () => {
         cy.visit('https://www.saucedemo.com/')     // Accede a la dirección del sitio indicado.
-        cy.get('#user-name').type('Navidad')       // Usa el metodo type, para insertar texto en el elemento
-        cy.get('#user-name').should('contain.value', 'idad')    // Usa el metodo should, con el argumento contain, para verificar que contenga el valor solicitado
+        cy.get('#user-name').type('standard_user')   // Usa el metodo type, para insertar texto en el elemento
+        cy.get('#password').type('secret_sauce')     // Usa el metodo type, para insertar texto en el elemento
+        cy.get('#login-button').click()              // Usa el metodo clic para clickear en el elemento
+        cy.get('#react-burger-menu-btn').click()    // Usa el metodo clic para clickear en el elemento
+        cy.get('.bm-menu-wrap').should('be.visible') // Usa el metodo should, con argumento 'be.visible' para ver si se muestra el elemento
         cy.wait(5000)
     })
 })
