6314358eac295c1d4a382858cfa4da6a4d5215c0
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-11-12 21:33:15 -0300

    Probando metodo should, con argumento be.checked, para verificar que el elemento este seleccionado

--- cypress/e2e/test-case.cy.js
+++ cypress/e2e/test-case.cy.js
@@ -1,11 +1,8 @@
 describe('test suite', () => {
     it('test case', () => {
-        cy.visit('https://www.saucedemo.com/')     // Accede a la dirección del sitio indicado.
-        cy.get('#user-name').type('standard_user')   // Usa el metodo type, para insertar texto en el elemento
-        cy.get('#password').type('secret_sauce')     // Usa el metodo type, para insertar texto en el elemento
-        cy.get('#login-button').click()              // Usa el metodo clic para clickear en el elemento
-        cy.get('#react-burger-menu-btn').click()    // Usa el metodo clic para clickear en el elemento
-        cy.get('.bm-menu-wrap').should('be.visible') // Usa el metodo should, con argumento 'be.visible' para ver si se muestra el elemento
+        cy.visit('https://the-internet.herokuapp.com/checkboxes')     // Accede a la dirección del sitio indicado.
+        cy.get('input[type="checkbox"]').eq(1).should('be.checked')   // Usa el metodo eq para  seleccionar la ubicacion del elemento, luego el metodo should, con argumento 'be.checked' para chequear que el elemento este seleccionado
+        
         cy.wait(5000)
     })
 })
